3b6bcd19a897bbb1ab6e04d56d33e543914c97e8
PolamW <pwong140@my.centennialcollege.ca>
2023-03-14 14:57:02 -0400

    Initial Commit

---
+++ MAPD724-W2023-ICE7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MAPD724-W2023-ICE7
+//
+//  Created by Po Lam Wong on 14/3/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
